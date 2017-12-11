22967e44079ad688ca8757a076a7af37eb7b8f2e
Dorothy Mitchell <dorwiz@anniecannons.com>
2017-12-11 14:39:17 -0800

    Completed circleCalculations

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -3,8 +3,15 @@
 //Bonus 1: Write the answer like this "The area of the circle is ___ and the perimeter of the circle is ____"
 
 //Bonus 2: Make both numbers whole integers (aka round to the nearest integer) 
+// Inupt: radius of the circle
+// Output: area and perimeter of circle
 
-function circleCalculation () {
+function circleCalculation (radius) {
 	//your code here
+	var area = Math.PI (radius * radius);
+	var perimeter = 2 * Math.PI * radius;
+	document.write (" The area  of the circle is " + area + " and the perimeter of the circle is " +  perimeter ")
+}
+
 
 circleCalculation(10);
